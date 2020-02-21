37f478c6441a656937bcde8e3d3690317fb199dd
asgharifard <khosro.question@gmail.com>
2020-02-21 21:49:24 +0330

    Fix some startup

--- bitcoin/bitcoinclicmd/bitcoinclicmd.bat
+++ bitcoin/bitcoinclicmd/bitcoinclicmd.bat
@@ -1,5 +1,5 @@
 rem echo %CD%
 rem set BitCoin_Run_HOME=%CD%
-bitcoin-cli   -conf="%BitCoin_Data_Home%\bitcoin.conf"   -datadir="%BitCoin_Data_Home%\bitcoind" -rpcport=43772  %*
+"%BitCoin_Home%\bin\bitcoin-cli"   -conf="%BitCoin_Data_Home%\bitcoin.conf"   -datadir="%BitCoin_Data_Home%\bitcoind" -rpcport=43772  %*
 
 rem cmd /k
